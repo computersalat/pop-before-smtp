e61acbc4d51341ebf7e76ee16bbdc0244b5b8dfb
Wayne Davison <wayned@users.sourceforge.net>
2003-02-01 09:56:08 +0000

    - Tweaked the Courier POP/IMAP pattern so that it will match a modern   log entry. - Added an EOF comment that the "1;" line must remain last in the file.

--- pop-before-smtp-conf.pl
+++ pop-before-smtp-conf.pl
@@ -121,7 +121,7 @@ $log_func = \&syslog;
 
 # For Courier-POP3 and Courier-IMAP:
 #$pat = '^(... .. ..:..:..) \S+ (?:courier)?(?:pop3|imap)(?:login|d|d-ssl): ' .
-#    'LOGIN, user=\S+, ip=\[[:f]*(\d+\.\d+\.\d+\.\d+)\]$';
+#    'LOGIN, user=\S+, ip=\[[:f]*(\d+\.\d+\.\d+\.\d+)\]';
 
 # For qmail's pop3d:
 #$pat = '^(... .. ..:..:..) \S+ vpopmail\[\d+\]: ' .
@@ -480,4 +480,4 @@ if (defined($PID_pat) && defined($IP_pat) && defined($OK_pat)) {
 EOT
 }
 
-1;
+1; ## THIS LINE MUST REMAIN LAST IN THE FILE! ##
